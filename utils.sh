b24d3703dfad620eedd2c114046841e207ca965a
Bird <5488190+Bird-Kid@users.noreply.github.com>
2020-06-20 15:41:47 +0200

    Simplify various util fns

--- utils.sh
+++ utils.sh
@@ -35,19 +35,19 @@ function validateParamCount () {
 }
 
 function getDiskNumber () {
-	echo "$( diskutil info "$1" | grep 'Part of Whole' | rev | cut -d ' ' -f1 | rev )"
+	diskutil info "$1" | grep 'Part of Whole' | rev | cut -d ' ' -f1 | rev
 }
 
 function getCoreStoragePhysicalDiskNumber () {
-	echo "$( diskutil info "$1" | grep 'PV UUID' | rev | cut -d '(' -f1 | cut -d ')' -f2 | rev | cut -d 'k' -f2 | cut -d 's' -f1 )"
+	diskutil info "$1" | grep 'PV UUID' | rev | cut -d '(' -f1 | cut -d ')' -f2 | rev | cut -d 'k' -f2 | cut -d 's' -f1
 }
 
 function getAPFSPhysicalDiskNumber () {
-	echo "$( diskutil apfs list | grep -A 9 "Container $1 " | grep "APFS Physical Store" | rev | cut -d ' ' -f 1 | cut -d 's' -f 2 | cut -d 'k' -f 1 )"
+	diskutil apfs list | grep -A 9 "Container $1 " | grep "APFS Physical Store" | rev | cut -d ' ' -f 1 | cut -d 's' -f 2 | cut -d 'k' -f 1
 }
 
 function getEFIVolume () {
-	echo "$( diskutil list | grep "$1s" | grep "EFI" | rev | cut -d ' ' -f 1 | rev )"
+	diskutil list | grep "$1s" | grep "EFI" | rev | cut -d ' ' -f 1 | rev
 }
 
 function getEFIPartition () {
@@ -76,15 +76,15 @@ function getEFIPartition () {
 }
 
 function getDiskMountPoint () {
-	echo "$( diskutil info "$1" | grep 'Mount Point' | rev | cut -d ':' -f 1 | rev | awk '{$1=$1;print}' )"
+	diskutil info "$1" | grep 'Mount Point' | rev | cut -d ':' -f 1 | rev | awk '{$1=$1;print}'
 }
 
 function getEFIDirectoryHash () {
-	echo "$( find -s . -not -path '*/\.*' -type f \( ! -iname ".*" \) -print0 | xargs -0 shasum | shasum )"
+	find -s . -not -path '*/\.*' -type f \( ! -iname ".*" \) -print0 | xargs -0 shasum | shasum
 }
 
 function logEFIDirectoryHashDetails () {
-	echo "$( find -s . -not -path '*/\.*' -type f \( ! -iname ".*" \) -print0 | xargs -0 shasum )"
+	find -s . -not -path '*/\.*' -type f \( ! -iname ".*" \) -print0 | xargs -0 shasum
 }
 
 function collectEFIHash () {
@@ -97,18 +97,18 @@ function collectEFIHash () {
 }
 
 function getSystemBootVolumeName () {
-	echo "$( system_profiler SPSoftwareDataType | grep 'Boot Volume' | rev | cut -d ':' -f 1 | rev | awk '{$1=$1;print}' )"
+	system_profiler SPSoftwareDataType | grep 'Boot Volume' | rev | cut -d ':' -f 1 | rev | awk '{$1=$1;print}'
 }
 
 function getCurrentBootEFIVolumeUUID () {
-	echo "$( bdmesg | grep 'SelfDevicePath' | rev | cut -d ')' -f 2 | rev | cut -d ',' -f 3 )"
+	bdmesg | grep 'SelfDevicePath' | rev | cut -d ')' -f 2 | rev | cut -d ',' -f 3
 }
 
 function getDeviceIDfromUUID () {
-	echo "$( diskutil info "$1" | grep 'Device Identifier' | rev | cut -d ' ' -f 1 | rev )"
+	diskutil info "$1" | grep 'Device Identifier' | rev | cut -d ' ' -f 1 | rev
 }
 
 function getDiskIDfromUUID () {
 	echoLog "$1"
-	echo "$( diskutil info "$1" | grep 'Device Identifier' | rev | cut -d ' ' -f 1 | rev )"
+	diskutil info "$1" | grep 'Device Identifier' | rev | cut -d ' ' -f 1 | rev
 }
